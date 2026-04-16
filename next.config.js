28be2b1b7fdd8f889b49ad8c6d085d9a414c15a6
zeelrupapara <zeelrupapara@gmail.com>
2026-04-16 15:18:41 +0530

    Revert "Fix swiper module resolution for Vercel builds"
    This reverts commit b49b5a4daf9aa838295d35dda9cb127faa8f6aaf.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["swiper"],
@@ -11,28 +9,6 @@ const nextConfig = {
         hostname: "lh3.googleusercontent.com",
       },
     ],
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "swiper/react": path.resolve(
-        __dirname,
-        "node_modules/swiper/swiper-react.mjs"
-      ),
-      "swiper/modules": path.resolve(
-        __dirname,
-        "node_modules/swiper/modules/index.mjs"
-      ),
-      "swiper/css/pagination": path.resolve(
-        __dirname,
-        "node_modules/swiper/modules/pagination.css"
-      ),
-      "swiper/css": path.resolve(
-        __dirname,
-        "node_modules/swiper/swiper.css"
-      ),
-    };
-    return config;
   },
 };
 
